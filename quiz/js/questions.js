bf840106c66c20dd662de0363e5e8f6117f14ead
Diogo Ribeiro <diogoribeiro698@gmail.com>
2022-04-24 21:52:58 -0300

    update nos arquivos ensinamento.html e atividade.html

--- quiz/js/questions.js
+++ quiz/js/questions.js
@@ -1,72 +1,57 @@
-// creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
-    numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
-  },
-    {
-    numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
-    options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
-  },
-    {
-    numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
-  },
-    {
-    numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+  numb: 1,
+  question: "Com base nos conceitos de algoritmos, lógica de programação e linguagens de programação, é correto afirmar que:",
+  answer: "As linguagens de programação possibilitam a codificação de um determinado algoritmo, de acordo com um conjunto de regras específico.",
+  options: [
+    "Os algoritmos correspondem, estritamente, aos conjuntos de dados e informações que são tratados em um determinado processamento.",
+    "As linguagens de programação possibilitam a codificação de um determinado algoritmo, de acordo com um conjunto de regras específico.",
+    "Os três conceitos correspondem às únicas formas de representação da solução de um determinado problema.",
+    "Lógica de programação corresponde à representação gráfica de um determinado algoritmo."
+  ]
+},
+  {
+  numb: 2,
+  question: "Uma determinada empresa de software desenvolveu um programa no qual é solicitado para o usuário o valor do produto. Com esse valor, o programa consegue informar para o usuário quanto de imposto está embutido no preço do produto. Para que esse programa faça essa interação com o usuário é necessário utilizar que tipos de comando?",
+  answer: "Comandos de entrada e saída.",
+  options: [
+    "Comandos de entrada e saída.",
+    "Comandos de fluxograma.",
+    "Comandos de decisão.",
+    "Comandos de repetição."
+  ]
+},
+  {
+  numb: 3,
+  question: "A definição de algoritmos são sequências de passos finitos para atingir um objetivo bem definido. Com base nessa definição, marque a alternativa que apresenta uma sequência lógica para fritar ovo .",
+  answer: "Coloque manteiga na frigideira - quebre o ovo - despeje na frigideira - retire-o quando estiver frito.",
+  options: [
+    "Retire-o quando estiver frito - coloque manteiga na frigideira - quebre o ovo - quebre o ovo.",
+    "Quebre o ovo - retire-o quando estiver frito - coloque manteiga na frigideira - quebre o ovo.",
+    "Despeje-o  na frigideira - retire quando estiver frito  - coloque-o no prato - salpique queijo ralado.  ",
+    "Coloque manteiga na frigideira - quebre o ovo - despeje na frigideira - retire-o quando estiver frito."
+  ]
+},
+  {
+  numb: 4,
+  question: "Que tipo de dado é apropriado para armazenar apenas uma letra?",
+  answer: "Caractere.",
+  options: [
+    "String de caracteres.",
+    "Inteiro.",
+    "Caractere.",
+    "Real."
+  ]
+},
+  {
+  numb: 5,
+  question: "1. var 2. num: inteiro 3. inicio 4. leia(num) 5. se (num % 2) =/= 0 entao 6. escreva(X) 7. senao 8. escreva(Y) 9. fimse 10. fimalgoritmo Considerando-se a lógica do algoritmo anterior, é correto afirmar que, para todo valor de num(linha 4)?",
+  answer: "Igual a 2, escreve Y.",
+  options: [
+    "Maior que 2, escreve Y.",
+    "Menor que 2, escreve X.",
+    "Igual a 2, escreve Y.",
+    "Igual a 2, escreve X."
+  ]
+}
 ];
